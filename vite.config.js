ee970ce3b878a8370ec6b09ca10bee5e9f2c989d
Yanic <try_oloski@outlook.com>
2025-08-05 14:11:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Marco-Portfolio",
+  base: "/Marco-Portfolio/",
   server: {
     open: true,
     port: 3001,
